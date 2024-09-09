d6377addd927d4a43d25b1645cfe867dd1729b38
Balasaiteja <balasaiteha17@gmail.com>
2024-09-09 23:48:52 +0530

    all done,only numbers need to be added who won the most

--- Rock-Paper-scissors/script.js
+++ Rock-Paper-scissors/script.js
@@ -1,11 +1,34 @@
+// here in random function 0 = rock , 1 = paper , 2 = scissor
 let btns = document.querySelectorAll(".clicker");
+let result = document.querySelector(".move");
 
-btns.forEach((btn) => {
+btns.forEach((btn, index) => {
   btn.addEventListener("mouseover", () => {
-    btn.style.border = "5px solid rosybrown";
+    btn.style.border = "5px solid orange";
   });
 
   btn.addEventListener("mouseout", () => {
     btn.style.border = "none";
+  });
+
+  btn.addEventListener("click", () => {
+    let computer_move = Math.floor(Math.random() * 3);
+    console.log(`Button ${index} clicked`);
+    console.log(`Computer move: ${computer_move}`);
+
+    if (index === computer_move) {
+      result.innerHTML = "It's a draw";
+      result.style.background = "orange";
+    } else if (
+      (index === 0 && computer_move === 1) ||
+      (index === 1 && computer_move === 2) ||
+      (index === 2 && computer_move === 0)
+    ) {
+      result.innerHTML = "Computer won!!, You Lose";
+      result.style.background = "red";
+    } else {
+      result.innerHTML = "You Won!!";
+      result.style.background = "darkseagreen";
+    }
   });
 });
